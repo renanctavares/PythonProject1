bd8fdea82686651a0d9f6ce3f75b55467c963b61
Renan Costa Tavares <r.costa.tavares@gmail.com>
2021-04-20 16:30:18 -0300

    Add guessing number
    Guessing number, but not checking if input is really a number. The number to guess is fixed.

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,1 +1,16 @@
-print("Bem vindo ao jogo de Adivinhação!")
+print("*********************************")
+print("* Welcome to the Guessing Game! *")
+print("*********************************")
+
+secret_number = 43
+
+guess = input("Type an integer number of your choice: ")
+
+print("You typed ", guess)
+
+if secret_number == int(guess):
+    print("You chose the right number! Congrats!")
+else:
+    print("You chose the wrong number! Try again!")
+
+print("The End")
