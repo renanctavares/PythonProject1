2b189c8d68dbbb2e4b664806c4b9f9aaa5860b7e
Renan Costa Tavares <r.costa.tavares@gmail.com>
2021-04-22 17:20:20 -0300

    Add while loop with fixed nº of attempts

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,17 +3,23 @@ print("* Welcome to the Guessing Game! *")
 print("*********************************")
 
 secret_number = 43
+total_attempts = 3
+round = 1
 
-guess = input("Type an integer number of your choice: ")
+while round <= total_attempts:
+    print("Round", round, "of", total_attempts)
+    guess = input("Type an integer number of your choice: ")
 
-print("You typed ", guess)
-int_guess = int(guess)
+    print("You typed ", guess)
+    int_guess = int(guess)
 
-if secret_number == int_guess:
-    print("You chose the right number! Congrats!")
-elif int_guess > secret_number:
-    print("You chose the wrong number! You guessed a greater number!")
-else:
-    print("You chose the wrong number! You guessed a smaller number!")
+    if secret_number == int_guess:
+        print("You chose the right number! Congrats!")
+    elif int_guess > secret_number:
+        print("You chose the wrong number! You guessed a greater number!")
+    else:
+        print("You chose the wrong number! You guessed a smaller number!")
+
+    round = round + 1
 
 print("The End")
