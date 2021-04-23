54805a5c6ecd5b3fbd6c10b892a9ac3bc328f089
Renan Costa Tavares <r.costa.tavares@gmail.com>
2021-04-22 23:12:05 -0300

    Add break and continue

--- adivinhacao.py
+++ adivinhacao.py
@@ -6,14 +6,18 @@ secret_number = 43
 total_attempts = 3
 
 for attempt in range(1, total_attempts+1):
-    print("Round {} of {}".format(round, total_attempts))
-    guess = input("Type an integer number of your choice: ")
+    print("Round {} of {}".format(attempt, total_attempts))
+    guess = input("Type an integer number between 1 and 100: ")
 
     print("You typed ", guess)
     int_guess = int(guess)
+    if int_guess < 1 or int_guess > 100:
+        print("You must type a number between 1 and 100")
+        continue
 
     if secret_number == int_guess:
         print("You chose the right number! Congrats!")
+        break
     elif int_guess > secret_number:
         print("You chose the wrong number! You guessed a greater number!")
     else:
