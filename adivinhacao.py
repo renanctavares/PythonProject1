87f80e96b2329b9ac6aa173efa6d4dfe5638bf55
Renan Costa Tavares <r.costa.tavares@gmail.com>
2021-05-03 17:21:46 -0300

    Add Levels to game

--- adivinhacao.py
+++ adivinhacao.py
@@ -5,7 +5,28 @@ print("* Welcome to the Guessing Game! *")
 print("*********************************")
 
 secret_number = random.randrange(1, 101)
-total_attempts = 3
+
+print("(1) Easy")
+print("(2) Normal")
+print("(3) Hard")
+level = int(input("Choose a level: "))
+flag_level = False
+
+while (flag_level != True):
+    if(level == 1):
+        total_attempts = 20
+        flag_level = True
+    elif(level == 2):
+        total_attempts = 10
+        flag_level = True
+    elif(level == 3):
+        total_attempts = 5
+        flag_level = True
+    else:
+        print("(1) Easy")
+        print("(2) Normal")
+        print("(3) Hard")
+        level = int(input("Choose a level: "))
 
 for attempt in range(1, total_attempts+1):
     print("Round {} of {}".format(attempt, total_attempts))
