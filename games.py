a4c17ccd9b0ffd5bd139aceddb8a75c02e4b6ee3
Renan Costa Tavares <r.costa.tavares@gmail.com>
2021-05-10 16:45:32 -0300

    Add define function choose_game()

--- games.py
+++ games.py
@@ -1,24 +1,28 @@
 import guessing
 import hangman
 
-print("************************")
-print("* Welcome to My Games! *")
-print("************************")
+def choose_game():
+    print("************************")
+    print("* Welcome to My Games! *")
+    print("************************")
 
-game_flag = False
+    game_flag = False
 
-print("(1) The Hangman Game")
-print("(2) The Guessing Game")
-game = int(input("Choose your game: "))
+    print("(1) The Hangman Game")
+    print("(2) The Guessing Game")
+    game = int(input("Choose your game: "))
 
-while(game_flag != True):
-    if(game == 1):
-        hangman.play()
-        game_flag = True
-    elif(game == 2):
-        guessing.play()
-        game_flag = True
-    else:
-        print("(1) The Hangman Game")
-        print("(2) The Guessing Game")
-        game = int(input("Choose your game: "))
+    while(game_flag != True):
+        if(game == 1):
+            hangman.play()
+            game_flag = True
+        elif(game == 2):
+            guessing.play()
+            game_flag = True
+        else:
+            print("(1) The Hangman Game")
+            print("(2) The Guessing Game")
+            game = int(input("Choose your game: "))
+
+if __name__ == "__main__":
+    choose_game()
