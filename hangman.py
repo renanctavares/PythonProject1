410eeb6e22731283520157de619288a5231443de
Renan Costa Tavares <r.costa.tavares@gmail.com>
2021-07-07 14:51:20 -0300

    Add Hangman game loop

--- hangman.py
+++ hangman.py
@@ -3,6 +3,15 @@ def play():
     print("* Welcome to the Hangman Game! *")
     print("********************************")
 
+    secret_word = "banana"
+
+    hanged = False
+    guessed = False
+
+    #Loop for the game
+    while(not hanged and not guessed):
+        print("playing ...")
+
     print("The End!")
 
 if __name__ == "__main__":
