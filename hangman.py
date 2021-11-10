b6dcd6cfcbe8eabe1837c361bb039b37de9a67aa
Renan Costa Tavares <r.costa.tavares@gmail.com>
2021-11-09 21:55:51 -0300

    Add end for Hagman game

--- hangman.py
+++ hangman.py
@@ -3,11 +3,12 @@ def play():
     print("* Welcome to the Hangman Game! *")
     print("********************************")
 
-    secret_word = "banana"
+    secret_word = "banana".upper()
     guessed_letters = ["_", "_", "_", "_", "_", "_"]
 
     hanged = False
     guessed = False
+    failures = 0
 
     print(guessed_letters)
 
@@ -15,16 +16,25 @@ def play():
     while not hanged and not guessed:
 
         guess = input("Which letter?")
-        guess = guess.strip()
+        guess = guess.strip().upper()
 
-        index = 0
-        for letter in secret_word:
-            if guess.upper() == letter.upper():
-                guessed_letters[index] = guess
-            index = index + 1
+        if guess in secret_word:
+            index = 0
+            for letter in secret_word:
+                if guess == letter:
+                    guessed_letters[index] = guess
+                index += 1
+        else:
+            failures += 1
 
+        hanged = failures == 6
+        guessed = "_" not in guessed_letters
         print(guessed_letters)
 
+    if guessed:
+        print("Congratulations! You won!")
+    else:
+        print("Oh I'm sorry! You lost!")
     print("The End!")
 
 if __name__ == "__main__":
